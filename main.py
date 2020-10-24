358cdaeb0e692d8c3925bc519cf86fb779f6138d
f3l0x1 <stachiwf@gmail.com>
2020-10-24 18:55:53 +0200

    Hello World

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World')
+print('')
+
+greeting = 'Hello'
+
+name = 'Niklas'
+
+message = f'{greeting}, {name}. Welcome!'
+
+print(message)
+
+num = 3.14
+
+print(type(num))
+
+print(3 + 3)
+
+print(18 % 3)
