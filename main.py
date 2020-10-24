0529781093496c02710e0f8462385efdc0663e91
f3l0x1 <stachiwf@gmail.com>
2020-10-24 21:29:13 +0200

    Hello World

--- main.py
+++ main.py
@@ -1,17 +1,73 @@
 print('')
 
 greeting = 'Hello'
-
 name = 'Niklas'
-
 message = f'{greeting}, {name}. Welcome!'
-
 print(message)
 
-num = 3.14
+print('')
 
+num = 3.14
 print(type(num))
+
+print('')
 
 print(3 + 3)
 
+print('')
+
 print(18 % 3)
+
+print('')
+
+print(round(3.75, ))
+
+print('')
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+courses_2 = ['Chemestry', 'PE', 'German']
+courses.extend(courses_2)
+print(courses)
+
+print('')
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+# nums = [1, 5, 3, 4, 2]
+sorted_courses = sorted(courses)
+# print(nums)
+print(sorted_courses)
+
+print('')
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+for index, course in enumerate(courses, start=1):
+    print(index, course)
+
+print('')
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+course_str = ' - '.join(courses)
+print(course_str)
+
+print('')
+
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+print(list_1)
+print(list_2)
+list_1[0] = 'Art'
+print(list_1)
+print(list_2)
+
+# tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+# tuple_2 = tuple_1
+# print(tuple_1)
+# print(tuple_2)
+# tuple_1[0]= 'Art'
+# print(tuple_1)
+# print(tuple_2)
+
+print('')
+
+cs_courses = {'History', 'Math', 'Physics', 'CombSci', 'Math'}
+print(cs_courses)
